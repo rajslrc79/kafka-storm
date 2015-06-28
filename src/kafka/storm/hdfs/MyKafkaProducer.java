e0d3cbfebca7631b71a172c05cdb5b2037037c97
System Administrator <prithviraj528@gmail.com>
2015-06-27 22:12:29 -0500

    commit

--- src/kafka/storm/hdfs/MyKafkaProducer.java
+++ src/kafka/storm/hdfs/MyKafkaProducer.java
@@ -36,7 +36,7 @@ public class MyKafkaProducer {
     
 
     public static void main(String[] args) throws Exception {
-    	
+    	//git testing
     	//turn off sl4j warnings
     	ch.qos.logback.classic.Logger rootLogger = (ch.qos.logback.classic.Logger)LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
     	rootLogger.setLevel(Level.toLevel("info"));
